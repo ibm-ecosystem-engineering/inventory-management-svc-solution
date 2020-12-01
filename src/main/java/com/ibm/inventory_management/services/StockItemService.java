b6f21ed304829fb0fb3a49a7b8dff0c2ce55242e
MuthuSundaravadivel <muthu.infoavc@gmail.com>
2020-12-01 21:47:13 +0530

    changes

--- src/main/java/com/ibm/inventory_management/services/StockItemService.java
+++ src/main/java/com/ibm/inventory_management/services/StockItemService.java
@@ -12,29 +12,14 @@ import com.ibm.inventory_management.models.StockItem;
 @Service
 @Primary
 public class StockItemService implements StockItemApi {
-    @Override
-    public List<StockItem> listStockItems() {
-        return asList(
-                new StockItem("1")
-                        .withName("Item 1")
-                        .withStock(100)
-                        .withPrice(10.5)
-                        .withManufacturer("Sony"),
-                new StockItem("2")
-                        .withName("Item 2")
-                        .withStock(150)
-                        .withPrice(100.0)
-                        .withManufacturer("Insignia"),
-                new StockItem("3")
-                        .withName("Item 3")
-                        .withStock(10)
-                        .withPrice(1000.0)
-                        .withManufacturer("Panasonic"),
-                new StockItem("4")
-                        .withName("Item 4")
-                        .withStock(9)
-                        .withPrice(990.0)
-                        .withManufacturer("JVC")
-        );
-    }
+        @Override
+        public List<StockItem> listStockItems() {
+                return asList(new StockItem("1").withName("TV").withStock(100).withPrice(10.5).withManufacturer("Sony"),
+                                new StockItem("2").withName("Moto 360").withStock(150).withPrice(100.0)
+                                                .withManufacturer("Insignia"),
+                                new StockItem("3").withName("AC").withStock(10).withPrice(1000.0)
+                                                .withManufacturer("Panasonic"),
+                                new StockItem("4").withName("Iwatch").withStock(9).withPrice(990.0)
+                                                .withManufacturer("JVC"));
+        }
 }
